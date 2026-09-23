7e6f85235e3afbdb2071387b6e367e37c7b89aa4
Shlok Nikam <shlokn678@gmail.com>
2026-09-23 01:19:02 -0400

    Package application as EmotionDetection

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,38 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=myobj, headers=header)
+
+    formatted_response = json.loads(response.text)
+    emotion_predictions = formatted_response['emotionPredictions'][0]['emotion']
+
+    anger = emotion_predictions['anger']
+    disgust = emotion_predictions['disgust']
+    fear = emotion_predictions['fear']
+    joy = emotion_predictions['joy']
+    sadness = emotion_predictions['sadness']
+
+    emotions = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    output = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
+
+    return output
---
+++ test_import.py
@@ -0,0 +1,4 @@
+from EmotionDetection import emotion_detector
+
+result = emotion_detector("I hate working long hours.")
+print(result)
